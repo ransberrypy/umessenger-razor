1ab2e63b62f7b96ac896f5cf0b87d1900dca60c0
Ransberrypy <Ransberrypy@gmail.com>
2021-06-21 17:02:11 +0000

    CONFIGURATION HEROKU

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import path from 'path'
 import dotenv from 'dotenv'
 import connectDB from './config/db.js'
 import contactRoutes from './routes/contactRoutes.js'
@@ -13,12 +14,19 @@ const app = express()
 
 app.use(express.json())
 
-app.get('/',(req, res)=>{
-    res.send("API running")
-})
+const __dirname = path.resolve()
 
 app.use('/api/contacts',contactRoutes)
 app.use('/api/users',userRoutes)
 
+
+if (process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname,'/frontend/build')))
+    app.get('*',(req, res) => res.sendFile(path.resolve(__dirname, "frontend",'build', 'index.html')))
+}else{
+    app.get('/',(req, res)=>{
+        res.send("API running")
+    })
+}
 
 app.listen(5000, console.log("Serer running on port 5000"))
